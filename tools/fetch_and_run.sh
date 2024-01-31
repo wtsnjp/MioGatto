bd6d292ee30d62c585b2e4a4d461cae5101a6eb0
delta-river <taigarana@gmail.com>
2024-01-31 12:57:21 +0900

    fix data file settting

--- tools/fetch_and_run.sh
+++ tools/fetch_and_run.sh
@@ -8,6 +8,7 @@ paper_id=$(echo ${arxiv_id} | sed -e "s/\///g")
 arxmliv_dir="./arxmliv"
 template_dir="./templates"
 source_dir="./sources"
+data_dir="./data"
 
 arxmliv_filename=${arxmliv_dir}/${paper_id}.html
 
@@ -26,8 +27,12 @@ python -m tools.fetch_html --arxmliv-dir ${arxmliv_dir} ${arxiv_id}
 
 python -m tools.preprocess --data=${template_dir} --sources=${source_dir} ${arxmliv_filename}
 
+# Copy the templates to data directory.
+cp ${template_dir}/${paper_id}_anno.json ${data_dir}/
+cp ${template_dir}/${paper_id}_mcdict.json ${data_dir}/
+
 #########################
 ## Run MioGatto Server ##
 #########################
 
-python -m server --data=${template_dir} --sources=${source_dir} ${paper_id}
+python -m server --data=${data_dir} --sources=${source_dir} ${paper_id}
